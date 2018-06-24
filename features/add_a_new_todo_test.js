a46525933d325e3fe47ea08b551f93a59396f9bb
Stefan Schenk <swj.schenk@portofrotterdam.com>
2018-06-24 17:38:03 +0200

    Created method in steps file to add and remove a todo

--- features/add_a_new_todo_test.js
+++ features/add_a_new_todo_test.js
@@ -4,25 +4,9 @@ Scenario('Add a new todo and cleanup at the end', (I) => {
     I.amOnPage('/');
 
     // Add a new to-do
-    I.fillField('input#new-todo',
-        'Create workshop introduction CodeceptJS');
-    I.pressKey('Enter');
+    I.addNewTodo('Create workshop introduction CodeceptJS');
 
-    // Make a screenshot
-    I.saveScreenshot('todo-created.png');
-
-    // Assert the to-do is created
-    I.see('introduction CodeceptJS');
-
-    // Cleanup
-    I.moveCursorTo('div.view label');
-    I.click('.destroy');
-
-    // Make another screenshot
-    I.saveScreenshot('todo-removed.png');
-
-    // Assert the to-do is deleted
-    I.dontSee('introduction CodeceptJS');
+    I.removeTodo();
 
     I.addMochawesomeContext({title: 'Test completed',
         value: './todo-created.png'});
--- steps_file.js
+++ steps_file.js
@@ -8,5 +8,29 @@ module.exports = function() {
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
 
+    // Add a new to-do
+    addNewTodo: function (todo) {
+      this.fillField('input#new-todo', todo);
+      this.pressKey('Enter');
+
+      // Make a screenshot
+      this.saveScreenshot('todo-created.png');
+
+      // Assert the to-do is created
+      this.see('introduction CodeceptJS');
+    },
+
+    // Remove a to-do
+    removeTodo: function () {
+      // Cleanup
+      this.moveCursorTo('div.view label');
+      this.click('.destroy');
+
+      // Make another screenshot
+      this.saveScreenshot('todo-removed.png');
+
+      // Assert the to-do is deleted
+      this.dontSee('introduction CodeceptJS');
+    }
   });
-}
+};
